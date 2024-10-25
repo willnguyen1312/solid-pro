4080a88a7154414141100d9ebb5f895859098a3f
Nam Nguyen <nam.nguyen@shopify.com>
2024-10-25 13:37:22 -0600

    Amazing Solid ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import { createSignal } from "solid-js";
+import { batch, createSignal } from "solid-js";
 
 export function App() {
-  const [count, setCount] = createSignal(1);
-  const increment = () => setCount((count) => count + 1);
+  const [count1, setCount1] = createSignal(1);
+  const increment1 = () => setCount1(count1() + 1);
+
+  const [count2, setCount2] = createSignal(1);
+  const increment2 = () => setCount2(count2() + 1);
 
   return (
-    <button type="button" onClick={increment}>
-      {count()}
-    </button>
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          increment1();
+        }}
+      >
+        {count1()}
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          increment2();
+        }}
+      >
+        {count2()}
+      </button>
+
+      <button
+        onclick={() => {
+          // batch(() => {
+          //   setCount1(count1() + 1);
+          //   debugger;
+          //   setCount2(count2() + 1);
+          // });
+
+          setCount1(count1() + 1);
+          debugger;
+          setCount2(count2() + 1);
+        }}
+      >
+        Increment all
+      </button>
+    </>
   );
 }
